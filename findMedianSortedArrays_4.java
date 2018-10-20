5db6ec783678bc10dda81dfebdbaf7364e45e8c6
AddictedSuzy <qq2439521261@gmail.com>
2018-10-19 21:43:15 -0500

    Revert "MergeSort"
    This reverts commit 5b502a7db807d6c7288cfd1e576fa75cdd913c7c.

--- findMedianSortedArrays_4.java
+++ findMedianSortedArrays_4.java
@@ -10,13 +10,6 @@ import java.util.LinkedList;
 /**
  *
  * @author Earl_iu
- * There are two sorted arrays nums1 and nums2 of size m and n respectively.
- * Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
- * You may assume nums1 and nums2 cannot be both empty.
- * Example 1:
- *   nums1 = [1, 3]  
- *   nums2 = [2]
- *   The median is 2.0
  */
 public class findMedianSortedArrays_4 {
     public static void main(String args[]){
