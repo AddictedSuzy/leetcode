d49583019d3992045b18b3b8014a720824b4beac
AddictedSuzy <qq2439521261@gmail.com>
2018-10-31 22:05:12 -0500

    动态规划

---
+++ RegularExpressionMatching_10.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package leetcode;
+
+
+/**
+ *
+ * @author Earl_iu
+ */
+public class RegularExpressionMatching_10 {
+    public static void main(String args[]){
+        String s = "aab";
+        String p = "c*a*b";
+        System.out.println(isMatch(s,p));
+    }
+    public static boolean isMatch(String s, String p) {
+          if (s == null || p == null) {
+            return false;
+            }
+        boolean[][] dp = new boolean[s.length()+1][p.length()+1];
+        dp[0][0] = true;
+        for (int i = 0; i < p.length(); i++) {
+            if (p.charAt(i) == '*' && dp[0][i-1]) {
+                dp[0][i+1] = true;
+            }
+        }
+        for (int i = 0 ; i < s.length(); i++) {
+            for (int j = 0; j < p.length(); j++) {
+                if (p.charAt(j) == '.') {
+                    dp[i+1][j+1] = dp[i][j];
+                }
+                if (p.charAt(j) == s.charAt(i)) {
+                    dp[i+1][j+1] = dp[i][j];
+                }
+                if (p.charAt(j) == '*') {
+                    if (p.charAt(j-1) != s.charAt(i) && p.charAt(j-1) != '.') {
+                        dp[i+1][j+1] = dp[i+1][j-1];
+                    } else {
+                        dp[i+1][j+1] = ( dp[i][j+1] || dp[i+1][j-1]);
+                    }
+                }
+            }
+        }
+        return dp[s.length()][p.length()];
+    }
+}
