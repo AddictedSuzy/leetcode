523607ae9bb98e790e2a12223a13ebc47c50b13f
AddictedSuzy <qq2439521261@gmail.com>
2018-10-31 22:04:43 -0500

    最长公共前缀

---
+++ CommonPrefix_14.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package leetcode;
+
+/**
+ *
+ * @author Earl_iu
+ * 最长公公前缀
+ */
+public class CommonPrefix_14 {
+    public static void main(String agrs[]){
+        String[] s = {"aaa","aa","aaa"};
+        System.out.println(solution(s));
+    }
+    public static String solution(String[] s){
+        if(s.length ==0){
+            return "";
+        }
+        String pre = s[0];
+        for(int i =1;i<s.length;i++){
+            while(s[i].indexOf(pre) !=0){
+                pre = pre.substring(0,pre.length()-1);
+                if(pre.isEmpty()){
+                    return "";
+                }
+            }
+        }
+        return pre;
+    }
+}
