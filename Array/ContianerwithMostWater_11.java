9b0a13d6f582df267a0cb8dce50cf01e8c9a51e4
AddictedSuzy <qq2439521261@gmail.com>
2018-11-12 23:29:05 -0600

    two pointer

---
+++ Array/ContianerwithMostWater_11.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package leetcode.Array;
+
+/**
+ *
+ * @author Earl_iu
+ */
+public class ContianerwithMostWater_11 {
+    public int maxArea(int[] height) {
+        int maxarea = 0, l = 0, r = height.length - 1;
+        while (l < r) {
+            maxarea = Math.max(maxarea, Math.min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r])
+                l++;
+            else
+                r--;
+        }
+        return maxarea;
+    }
+}
